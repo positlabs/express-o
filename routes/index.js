7d089f9e6c3732bd4cd3e867eb60e8f49c6e6bba
jbeckwith <josh.beckwith@toolofna.com>
2016-11-15 12:37:18 -0800

    add ENV, CDN vars. add some og meta tags

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const router = express.Router()
 const app = express()
 
+var ENV = app.get('env')
+var CDN = '/'
+if(ENV !== 'local') CDN = '/' // TODO: point to your CDN!
+
 router.get('/', (req, res, next) => {
 	res.render('index', { 
 		title: 'express-o',
-		env: app.get('env')
+		ENV,
+		CDN
 	})
 })
 
