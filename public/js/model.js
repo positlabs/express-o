afc3dcebc2d8580d3a7e593c6b135c8f6a2ebb86
jbeckwith <josh.beckwith@toolofna.com>
2016-11-15 16:34:43 -0800

    using model-o

--- public/js/model.js
+++ public/js/model.js
@@ -1,69 +1,11 @@
-const EventEmitter = require('wolfy87-eventemitter')
 
 /*
-
-	Simple model that supports shallow property change watchers
-
-	NOTE: all values must be defined in the object passed to the constructor!
-			... otherwise, use model.define(prop, value) to activate events
-
-	var model = new Model({
-		str: 'hello',
-		obj: {
-			a: 'a' // NOTE: unwatchable
-		},
-		subModel: new Model({
-			a: 'a' // watchable
-		}),
-		bool: true,
-		num: 123.4567,
-		arr: [1, 2, 3, 4, 5]
-	})
-
-	console.log(model)
-
-	var onChange = function(newval, oldval){
-		console.log('newval: ' + newval, ', oldval: ' + oldval)
-	}
-	model.on('str', onChange)
-	model.str = 'asdf'
-	model.str += 'asdf'
-
-	model.subModel.watch('a', onChange)
-	model.subModel.a = 'A'
-
-	model.on('arr', onChange)
-	model.arr = model.arr.concat('extra')
-
+	singleton model for the app
 */
-class Model extends EventEmitter {
-	
-	constructor(obj){
-		super()
-		this._properties = Object.assign({}, obj) // clone just to be safe
-		Object.keys(this._properties).forEach(key => {
-			this.define(key, this._properties[key])
-		})
-	}
 
-	define(key, defaultValue){
-		
-		this._properties[key] = defaultValue
+const Model = require('model-o')
+const model = new Model({
 
-		Object.defineProperty(this, key, {
-			get(){ return this._properties[key] },
-			set(newValue){
-				// ignore if value is unchanged
-				// only works for primitives
-				if(newValue === this._properties[key]) return
-				var oldValue = this._properties[key]
-				this._properties[key] = newValue
-				this.emit(key, newValue, oldValue)
-			},
-			enumerable: true
-		})
-	}
-}
+})
 
-module.exports = Model
-
+module.exports = model
