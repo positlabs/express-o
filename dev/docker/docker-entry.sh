beeb8d564e23eb9efb5682b821a556b14547eb56
jbeckwith <josh.beckwith@toolofna.com>
2017-10-18 13:17:25 -0700

    fix path

--- dev/docker/docker-entry.sh
+++ dev/docker/docker-entry.sh
@@ -3,7 +3,7 @@ if [ "$NODE_ENV" == "local" ] ; then
     # dev command
     echo "docker-entry.sh running pm2 start"
     yarn global add pm2
-    pm2 start ./renderer/dev/env/local.yaml && bash;
+    pm2 start ./dev/env/local.yaml && bash;
 else
     # production command
     echo "docker-entry.sh running npm start"
